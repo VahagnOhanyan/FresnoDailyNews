911e3e7422748e8ae2751a30ae34441ed559031c
Vahagn Ohanyan <vahagn.ohanyan.2908@gmail.com>
2023-04-21 20:06:31 -0700

    get api_key from app config file

--- src/main/java/org/fresno/adapter/perigon/api/GetFresnoNewsService.java
+++ src/main/java/org/fresno/adapter/perigon/api/GetFresnoNewsService.java
@@ -1,9 +1,11 @@
 package org.fresno.adapter.perigon.api;
+
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.ListenableFuture;
 import org.asynchttpclient.Response;
 import org.fresno.repo.NewsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,13 +16,15 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class GetFresnoNewsService implements NewsService {
     private final SimpleDateFormat dashDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    @Value("${perigon.api_key}")
+    String apiKey;
 
     public Response get() throws IOException {
         Date date = new Date();
         String dateInDashFormat = dashDateFormat.format(date);
         AsyncHttpClient client = new DefaultAsyncHttpClient();
         try (client) {
-            ListenableFuture<Response> res = client.prepare("GET", "https://api.goperigon.com/v1/all?topic=Fresno&apiKey=2d397e58-36c6-40a4-b277-175612b29052&from=" + dateInDashFormat + "&to=" + dateInDashFormat + "&language=en&sortBy=relevance&showReprints=true")
+            ListenableFuture<Response> res = client.prepare("GET", "https://api.goperigon.com/v1/all?topic=Fresno&apiKey=" + apiKey + "&from=" + dateInDashFormat + "&to=" + dateInDashFormat + "&language=en&sortBy=relevance&showReprints=true")
                     .setHeader("accept", "application/json")
                     .setHeader("Accept", "application/json")
                     .execute();
